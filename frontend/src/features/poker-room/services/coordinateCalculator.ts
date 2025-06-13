93ac8bc673de91cbecaf1f883864373c93eb87de
Artyom Antonov <artemkauser@yandex.ru>
2025-06-13 14:38:45 +0300

    Add dynamic user places which depends on amount of users

--- frontend/src/features/poker-room/services/coordinateCalculator.ts
+++ frontend/src/features/poker-room/services/coordinateCalculator.ts
@@ -1,19 +1,31 @@
 import {Coordinate} from "../models/coordinate"
 
-
 export class CoordinateCalculator {
-    constructor(
-        private _radius: number) {
-    }
+    calculateCoordinate(userAmount: number): Coordinate[] {
+        const a = userAmount <= 4 ? 200 :
+            userAmount >= 8 ? 400 :
+                200 + (userAmount - 4) * 50;
 
-    calculateCoordinate(angle: number): Coordinate {
-        return {
-            x: this._radius * Math.cos(this.angleToRadians(angle)),
-            y: this._radius * Math.sin(this.angleToRadians(angle))
+        const b = 200;
+
+        const result: Coordinate[] = [];
+
+        const angleStep = 360 / userAmount;
+        let currentAngle = 0;
+
+        for (let i = 0; i < userAmount; i++) {
+            result.push({
+                x: a * Math.cos(this.angleToRadians(currentAngle)),
+                y: b * Math.sin(this.angleToRadians(currentAngle))
+            });
+
+            currentAngle += angleStep;
         }
+
+        return result;
     }
 
-    angleToRadians(angle: number) {
+    private angleToRadians(angle: number) {
         return angle * Math.PI / 180
     }
 }
